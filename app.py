26bee5f5ea05322a94774a81c9442f34b4486595
SATHYA <santhoshajas235@gmail.com>
2024-03-25 18:51:22 +0530

    Working Model

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import os
+import helmet_detection
+import cv2
+
+def save_file(file, folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    with open(os.path.join(folder_path, file.name), "wb") as f:
+        f.write(file.getbuffer())
+    return os.path.join(folder_path, file.name)
+
+def pred_image(img_path):
+    frame = cv2.imread(img_path)
+    out_img,result = helmet_detection.predict_and_detect(frame)
+    out_img_path = "out_image.jpg"
+    cv2.imwrite(out_img_path, out_img)
+    return out_img_path
+
+def pred_vid(vid_path):
+    out_vid_path,res = helmet_detection.get_vid_predictions(vid_path)
+    print(out_vid_path)
+    return out_vid_path
+
+def main():
+
+    st.title("Detection App")
+    sub_option = st.selectbox("Choose detection method:", ("Image Prediction", "Video Prediction"))
+
+    if sub_option == "Image Prediction":
+        uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+        if uploaded_file is not None:
+            image_path = save_file(uploaded_file, "user_input")
+            if st.button('Predict', key='predict_button'):
+                with st.spinner('Processing image...'):
+                    result_path = pred_image(image_path)
+                    st.image(result_path, caption="Predicted Image")
+
+    else:
+        uploaded_file = st.file_uploader("Upload Video", type=["mp4"])
+        if uploaded_file is not None:
+            video_path = save_file(uploaded_file, "user_input")
+            if st.button('Predict', key='predict_button'):
+                with st.spinner('Processing video...'):
+                    result_path = pred_vid(video_path)
+                    st.video(result_path)
+
+    # Add custom CSS for styled button
+    predict_button_style = """
+        <style>
+            .predict-button {
+                background-color: #4CAF50;
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 2px;
+                cursor: pointer;
+                border-radius: 8px;
+            }
+        </style>
+    """
+    st.markdown(predict_button_style, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ helmet_detection.py
@@ -0,0 +1,95 @@
+from ultralytics import YOLO
+import os
+import cv2
+import shutil
+from datetime import datetime
+from tqdm.auto import tqdm
+import subprocess
+
+model = YOLO('best.pt',verbose=False)
+model2 = YOLO('license_plate_detector.pt',verbose=False)
+
+def predict(chosen_model, img, classes=[], conf=0.5):
+    if classes:
+        results = chosen_model.predict(img, classes=classes, conf=conf)
+    else:
+        results = chosen_model.predict(img, conf=conf)
+
+    return results
+
+def predict_and_detect(img, classes=[], conf=0.5):
+    results = predict(model, img, classes, conf=conf)
+    res = []
+    for result in results:
+        for box in result.boxes:
+            if result.names[int(box.cls[0])] != 'motorcyclist':
+                cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                            (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (0, 0, 255), 2)
+                cv2.putText(img, f"{result.names[int(box.cls[0])]}",
+                            (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                            cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+                res.append(result.names[int(box.cls[0])])
+
+    results = predict(model2, img, classes, conf=conf)
+    for result in results:
+        for box in result.boxes:
+            if result.names[int(box.cls[0])] != 'motorcyclist':
+                cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                            (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (0, 0, 255), 2)
+                cv2.putText(img, f"{result.names[int(box.cls[0])]}",
+                            (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                            cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+                res.append(result.names[int(box.cls[0])])
+
+    return img, res
+
+def create_video_writer(video_cap, output_filename):
+    frame_width = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(video_cap.get(cv2.CAP_PROP_FPS))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    writer = cv2.VideoWriter(output_filename, fourcc, float(fps), (frame_width, frame_height))
+
+    return writer
+
+def get_vid_predictions(vid_path):
+    result_score = []
+    folder_path = os.path.join('results', datetime.now().strftime("%d_%m_%Y_%H_%M_%S"))
+    os.makedirs(folder_path)
+
+    shutil.copy(vid_path, folder_path)
+    output_filename = os.path.join(folder_path, "output.mp4")
+
+    cap = cv2.VideoCapture(vid_path)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    writer = create_video_writer(cap, output_filename)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    with tqdm(total=total_frames, desc="Processing Frames") as pbar:
+        while True:
+            success, vid_frame = cap.read()
+
+            if not success:
+                break
+
+            result_img,_ = predict_and_detect(vid_frame, classes=[], conf=0.2)
+            if _:
+                result_score = _
+                cv2.imwrite('output_image.jpg', result_img)
+            writer.write(result_img)
+            pbar.update(1)
+
+        cap.release()  # Release the video capture
+        writer.release()  # Release the video writer
+
+    temp_file_result = output_filename
+    convertedVideo = os.path.join(folder_path,'output2.mp4')
+    subprocess.call(args=f"ffmpeg -y -i {temp_file_result} -c:v libx264 {convertedVideo}".split(" "))
+
+    return os.path.abspath(convertedVideo),result_score
+
+if __name__ == '__main__':
+    vid_path = 'sample.mp4'
+    path = get_vid_predictions(vid_path)
+    print(path)
